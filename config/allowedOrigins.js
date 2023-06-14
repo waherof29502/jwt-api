c0a72dd542f7c8f945aa29e05ccfda68bf225453
jinwangdev <jinwangdev>
2023-06-14 15:38:29 +0800

    finish project

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://www.dandrepairshop.com',
+  'https://dandrepairshop.com',
+];
+
+module.exports = allowedOrigins;
